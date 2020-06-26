bec9344a20ff56beb176048a41349f578d1701db
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-06-26 14:47:18 -0300

    Adicionando métodos na classe JogoDaVelha_PC

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -1,4 +1,14 @@
 
 public class JogoDaVelha_PC {
 
+  public JogoDaVelha_Mapa mapa;
+	char letra;
+	
+	public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
+		
+	}
+	
+	public static boolean joga() {
+		return true;
+	}
 }
