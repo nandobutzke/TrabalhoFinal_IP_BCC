9b03faa47ae1048588b78563fa1b7f8e03706f1a
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-06-26 14:46:18 -0300

    Adicionando métodos na classe JogoDaVelha_Jogador

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
@@ -1,4 +1,14 @@
 
 public class JogoDaVelha_Jogador {
 
+  public JogoDaVelha_Mapa mapa;
+	char letra;
+	
+	public JogoDaVelha_Jogador(JogoDaVelha_Mapa mapa) {
+		
+	}
+	
+	public static boolean joga(Scanner teclado) {
+		return true;
+	}
 }
