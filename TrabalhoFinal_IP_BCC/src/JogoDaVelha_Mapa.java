f02f376bfb3f319580945816ef541ff99501ca36
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-06-26 14:43:00 -0300

    Adicionando métodos na classe JogoDaVelha_Mapa

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Mapa.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Mapa.java
@@ -1,4 +1,25 @@
 
 public class JogoDaVelha_Mapa {
 
+  char[][] mapa = new char[2][2];
+	
+	public static int sortear(int inicio, int fim) {
+		return 1;
+	}
+	
+	public static void limpaMapa() {
+		
+	}
+	
+	public static void desenha(int jogada) {
+		
+	}
+	
+	public static boolean jogar(int i, int c, char jogador) {
+		return true;
+	}
+	
+	public static boolean ganhou(char jogador) {
+		return true;
+	}
 }
