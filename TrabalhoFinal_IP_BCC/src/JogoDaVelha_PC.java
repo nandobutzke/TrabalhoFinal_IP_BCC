0588e374a314ebabee8214060a07abd2e388728f
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-02 20:54:01 -0300

    Adicionando implementações na JogoDaVelha_PC

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -2,10 +2,20 @@
 public class JogoDaVelha_PC {
 
   public JogoDaVelha_Mapa mapa;
-	char letra;
+	public char letra;
 	
 	public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
+		char linha = 0;
+		char coluna = 0;
+		char letra[][] = new char[linha][coluna];
 		
+		
+		do {
+			linha = (char) Math.random();
+			coluna = (char) Math.random();
+		} while(letra[linha][coluna] != ' ');
+			letra[linha][coluna] = 'O';
+			
 	}
 	
 	public static boolean joga() {
