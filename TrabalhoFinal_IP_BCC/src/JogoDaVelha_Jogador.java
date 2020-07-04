83f9ab7fd3fda6316d4cf98bedcc2fbcbe11e950
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-04 17:48:38 -0300

    Atualizando JogoDaVelha_Jogador

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
@@ -9,6 +9,23 @@ public class JogoDaVelha_Jogador {
 	}
 	
 	public static boolean joga(Scanner teclado) {
+		boolean verify;
+		int row,
+		    col;
+
+		do {
+		    System.out.println("linha:");
+		    row = teclado.nextInt();
+		    System.out.println("coluna:");
+		    col = teclado.nextInt();
+		    if ((row <= 2) && (col <= 2)) {
+			verify = true;
+		    } else {
+			verify = false;
+		    }
+		} while (verify == false);
+
 		return true;
+	    }
 	}
 }
