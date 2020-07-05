0ef3b7e6475a5d88461cdd5056b43e06b207faa8
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-05 20:56:25 -0300

    Atualizando JogoDaVelha_Jogador

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 
 public class JogoDaVelha_Jogador {
 
-  public JogoDaVelha_Mapa mapa;
+	public JogoDaVelha_Mapa mapa;
     char letra = 'X';
     
     public JogoDaVelha_Jogador(JogoDaVelha_Mapa mapa) {
@@ -28,31 +29,39 @@ public class JogoDaVelha_Jogador {
         }
 
  	
-	// Não sei se essa porra vai dar certo   
+	// --------------------------------
 	
 	public static int vez(){
 	    
 		int testeJogada = JogoDaVelha_Jogador.vez();
 		
-		if(testeJogada %2 == 1)
+		if(testeJogada % 2 == 1)
 	        return 1;
 	    else
 	        return 2;
 		}
-	// essa porra de cima
+	// ---------------------------------
 
- 
-
- 	public int ganhou(){
+ 	public static boolean checaGanhador(){
 	 	
- 		if(mapa.checaLinhas() == 1 && mapa.checaColunas() == 1 && mapa.checaDiagonais() == 1)
+ 		if(JogoDaVelha_Mapa.checaLinhas() == 1 && JogoDaVelha_Mapa.checaColunas() == 1 && JogoDaVelha_Mapa.checaDiagonais() == 1)
+	        return true;
+	    
+	    if(JogoDaVelha_Mapa.checaLinhas() == -1 && JogoDaVelha_Mapa.checaColunas() == -1 && JogoDaVelha_Mapa.checaDiagonais() == -1)
+	        return false;
+    
+	    return false;
+ 	}
+ 	
+ 		/* public static int checaGanhador(){
+	 	
+ 		if(JogoDaVelha_Mapa.checaLinhas() == 1 && JogoDaVelha_Mapa.checaColunas() == 1 && JogoDaVelha_Mapa.checaDiagonais() == 1)
 	        return 1;
 	    
-	    if(mapa.checaLinhas() == -1 && mapa.checaColunas() == -1 && mapa.checaDiagonais() == -1)
+	    if(JogoDaVelha_Mapa.checaLinhas() == -1 && JogoDaVelha_Mapa.checaColunas() == -1 && JogoDaVelha_Mapa.checaDiagonais() == -1)
 	        return -1;
     
-	    return 0;
-	}
+	    return 0; 
+ 	} */
 
- 
 }
