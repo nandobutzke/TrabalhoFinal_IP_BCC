bd48c800d0ae4a726219e53ff88d1b57caa16b76
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-04 21:57:00 -0300

    Atualizando JgoDaVelha_PC

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -1,24 +1,49 @@
 
 public class JogoDaVelha_PC {
 
-  public JogoDaVelha_Mapa mapa;
-	public char letra;
-	
-	public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
-		char linha = 0;
-		char coluna = 0;
-		char letra[][] = new char[linha][coluna];
+ public JogoDaVelha_Mapa mapa;
+		public char letra = 'O';
 		
-		
-		do {
-			linha = (char) Math.random();
-			coluna = (char) Math.random();
-		} while(letra[linha][coluna] != ' ');
-			letra[linha][coluna] = 'O';
+		public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
+			char linha = 0;
+			char coluna = 0;
+			char letra[][] = new char[linha][coluna];
 			
-	}
-	
-	public static boolean joga() {
-		return true;
-	}
+			
+			do {
+				linha = (char) Math.random();
+				coluna = (char) Math.random();
+			} while(letra[linha][coluna] != 0);
+		}
+		
+			public static int vez() {
+			
+			int testeJogada = JogoDaVelha_PC.vez();
+			
+			if(testeJogada % 2 == 1) {
+				return 1;
+			} else {
+				return 2;
+			}
+		}
+		
+		public static boolean joga() {
+			boolean verify;
+	        int row,
+	            col;
+	        
+	        do {
+	            System.out.println("linha:");
+	            row = (int) Math.random();
+	            System.out.println("coluna:");
+	            col = (int) Math.random();
+	            if ((row <= 2) && (col <= 2)) {
+	                verify = true;
+	            } else {
+	                verify = false;
+	            }
+	        } while (verify == false);
+	       
+	        return true;
+		}
 }
