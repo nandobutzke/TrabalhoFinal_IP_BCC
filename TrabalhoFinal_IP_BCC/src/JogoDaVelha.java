46e0d54a9b22b4b8256217d27d0a37acb49a5b0e
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-04 21:59:34 -0300

    Atualizando JogoDaVelha

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha.java
@@ -2,16 +2,52 @@ import java.util.Scanner;
 
 public class JogoDaVelha {
 
-	public JogoDaVelha_Mapa jogoMapa;
+	public static JogoDaVelha_Mapa jogoMapa;
 	public JogoDaVelha_PC jogoPC;
 	public JogoDaVelha_Jogador jogoJogador;
 	
 	public static void jogar(Scanner teclado) {
+		JogoDaVelha_Mapa.desenha(1);
 		
+		JogoDaVelha_Mapa.limpaMapa();
+		
+		int sorteio;
+		sorteio = JogoDaVelha_Mapa.sortear(1,2);
+		
+		if(sorteio == 1)
+			JogoDaVelha_Jogador.joga(teclado);
+	    else if(sorteio == 2)
+	    	JogoDaVelha_PC.joga();
+		
+		
+
 	}
 	
 	public static void main(String[] args) {	
-
+		Scanner tec = new Scanner(System.in);
+		
+		
+		JogoDaVelha_Mapa mapaJogo = new JogoDaVelha_Mapa();
+		//JogoDaVelha_PC pc = new JogoDaVelha_PC(jogoMapa);
+		//JogoDaVelha_Jogador jogador = new JogoDaVelha_Jogador(jogoMapa);
+		
+		
+		JogoDaVelha.jogar(tec);
+		
+		
+		
+		
+		//jogoMapa.ganhou('x');
+		//jogar(tec);
+		//char jogarNovamente = tec.next().charAt(0);
+		
+		//if(jogarNovamente == 's')
+		
+		
+		
+		
+		
+		tec.close();
 	}
 
 }
