4a6a4c3092bff6cc695754d44dac41cc5c39174b
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-07 16:56:37 -0300

    Atualizando PC

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -1,43 +1,15 @@
+
 public class JogoDaVelha_PC {
 
 		public JogoDaVelha_Mapa mapa;
 		public static char letra = 'O';		//Usar letra 'O'
 		
 		public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-//			char linha = 0;
-//			char coluna = 0;
-//			char letra[][] = new char[linha][coluna];
-//			
-//			
-//			do {
-//				linha = (char) Math.random();
-//				coluna = (char) Math.random();
-//			} while(letra[linha][coluna] != 0);
+			this.mapa = mapa;
 		}
+
 		
-		public static int vez() {
-			
-			int testeJogada = JogoDaVelha_PC.vez();
-			
-			if(testeJogada % 2 == 1) {
-				return 1;
-			} else {
-				return 2;
-			}
-		}
-		
-		public static boolean joga() {
+		public boolean joga() {
 			boolean verify;
 	        int row,
 	            col;
@@ -54,28 +26,5 @@ public class JogoDaVelha_PC {
 	            }
 	        } while (verify == false);
 	        	return true;
-			}
-		
-		public static boolean checaGanhador(){
-		 	
-	 		if(JogoDaVelha_Mapa.checaLinhas() == 1 && JogoDaVelha_Mapa.checaColunas() == 1 && JogoDaVelha_Mapa.checaDiagonais() == 1)
-		        return true;
-		    
-		    if(JogoDaVelha_Mapa.checaLinhas() == -1 && JogoDaVelha_Mapa.checaColunas() == -1 && JogoDaVelha_Mapa.checaDiagonais() == -1)
-		        return false;
-	    
-		    return false;
 		}
-		
-		
-		/*public static int checaGanhador(){
-		 	
-	 		if(JogoDaVelha_Mapa.checaLinhas() == 1 && JogoDaVelha_Mapa.checaColunas() == 1 && JogoDaVelha_Mapa.checaDiagonais() == 1)
-		        return 1;
-		    
-		    if(JogoDaVelha_Mapa.checaLinhas() == -1 && JogoDaVelha_Mapa.checaColunas() == -1 && JogoDaVelha_Mapa.checaDiagonais() == -1)
-		        return -1;
-	    
-		    return 0;
-		} */
 }
