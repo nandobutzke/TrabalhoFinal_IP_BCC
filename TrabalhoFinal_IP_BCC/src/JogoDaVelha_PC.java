26bebd65c23ecb2a201381c6d273405225336ea2
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-08 19:58:01 -0300

    Update JogoDaVelha_PC.java

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -1,30 +1,37 @@
 
 public class JogoDaVelha_PC {
 
-		public JogoDaVelha_Mapa mapa;
-		public static char letra = 'O';		//Usar letra 'O'
-		
-		public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
-			this.mapa = mapa;
-		}
+	public JogoDaVelha_Mapa mapa;
+	public static char letra = 'O';		//Usar letra 'O'
+	
+	public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
+		this.mapa = mapa;
+	}
 
+	
+	public boolean joga() {
 		
-		public boolean joga() {
-			boolean verify;
-	        int row,
-	            col;
-	        
-	        do {
-	            System.out.println("linha:");
-	            row = (int) Math.random();
-	            System.out.println("coluna:");
-	            col = (int) Math.random();
-	            if ((row <= 2) && (col <= 2)) {
-	                verify = true;
-	            } else {
-	                verify = false;
-	            }
-	        } while (verify == false);
-	        	return true;
-		}
+		mapa = new JogoDaVelha_Mapa();
+    	
+    	System.out.println("linha:");
+    	int linha = (int) Math.random();
+    	System.out.println("coluna:");
+    	int coluna = (int) Math.random();
+
+    	boolean verificar = mapa.jogar(linha, coluna, letra);
+    	
+    	if(linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+    		System.out.println("Fora da caixa!");
+    		joga();
+    		return false;
+    	}
+    	
+    	if(verificar) {
+    		System.out.println("jogada inválida, campo ocupado");
+    		joga();
+    	}
+    	
+    	
+    	return true;
+	}
 }
