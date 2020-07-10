78c35c31253b519533be3b38291789c4939dc74e
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-09 21:57:00 -0300

    Update JogoDaVelha_Mapa.java

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Mapa.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Mapa.java
@@ -1,62 +1,73 @@
 public class JogoDaVelha_Mapa {
+    public static char[][] mapa = new char[3][3];
 
-	    public static char[][] mapa = new char[3][3];
+ 
 
-	    public int sortear(int inicio, int fim) {
-			return (int) ((Math.random()*fim)-inicio);
-		}
-	    
-	    public void limpaMapa(){
-	        for(int linha = 0; linha < 3; linha++)
-	            for(int coluna = 0; coluna <3; coluna++)
-	                mapa[linha][coluna] = ' ';
-	    }
+    public int sortear(int inicio, int fim) {
+        return (int) ((Math.random()*fim)-inicio);
+    }
+    
+    public void limpaMapa(){
+        for(int linha = 0; linha < 3; linha++)
+            for(int coluna = 0; coluna <3; coluna++)
+                mapa[linha][coluna] = ' ';
+    }
 
-	    public void desenha(int jogada){
-	        
-	        System.out.print("-------------");
-	        System.out.println(" jogada: " + jogada);
-	        System.out.println("| " + mapa[0][0] + " | " + mapa[0][1] + " | " + mapa[0][2] + " | ");
-	        System.out.println("-------------");
-	        System.out.println("| " + mapa[1][0] + " | " + mapa[1][1] + " | " + mapa[1][2] + " | ");
-	        System.out.println("-------------");
-	        System.out.println("| " + mapa[2][0] + " | " + mapa[2][1] + " | " + mapa[2][2] + " | ");
-	        System.out.println("-------------");
-	        
-	    }
+ 
 
-	    public boolean jogar(int l, int c, char jogador) {
+    public void desenha(int jogada){
+        
+        System.out.print("-------------");
+        System.out.println(" jogada: " + jogada);
+        System.out.println("| " + mapa[0][0] + " | " + mapa[0][1] + " | " + mapa[0][2] + " | ");
+        System.out.println("-------------");
+        System.out.println("| " + mapa[1][0] + " | " + mapa[1][1] + " | " + mapa[1][2] + " | ");
+        System.out.println("-------------");
+        System.out.println("| " + mapa[2][0] + " | " + mapa[2][1] + " | " + mapa[2][2] + " | ");
+        System.out.println("-------------");
+        
+    }
 
-			if(mapa[l][c] == ' ') {
-				mapa[l][c] = jogador;
-				return true;
-			} else {
-				return false;
-			}
+ 
 
-		}
+    public boolean jogar(int l, int c, char jogador) {
 
-	    public boolean ganhou(char jogador){
-	        //for(int linha = 0; linha < 3; linha++)
-	            //for(int coluna = 0; coluna < 3 ; coluna++)
-	                
-	    			//linha
-	    			if((mapa[0][0] == jogador && mapa[1][0] == jogador && mapa[2][0] == jogador) ||
-	                	(mapa[0][1] == jogador && mapa[1][1] == jogador && mapa[2][1] == jogador) ||
-	                	(mapa[0][2] == jogador && mapa[1][2] == jogador && mapa[2][2] == jogador))
-	                     return true;
-	                
-	    			//coluna
-	                if((mapa[0][0] == jogador && mapa[0][1] == jogador && mapa[0][2] == jogador) ||
-		                (mapa[0][1] == jogador && mapa[1][1] == jogador && mapa[1][2] == jogador) ||
-		                (mapa[2][0] == jogador && mapa[2][1] == jogador && mapa[2][2] == jogador))
-		                 return true;
-	                
-	                //diagonal
-	                if((mapa[0][0] == jogador && mapa[1][1] == jogador && mapa[2][2] == jogador) ||
-		                (mapa[2][0] == jogador && mapa[1][1] == jogador && mapa[0][2] == jogador))
-		                 return true;
-	        return false;
-	   }
+ 
+
+        if(mapa[l][c] == ' ') {
+            mapa[l][c] = jogador;
+            return true;
+        } else {
+            return false;
+        }
+
+ 
+
+    }
+
+ 
+
+    public boolean ganhou(char jogador){
+        //for(int linha = 0; linha < 3; linha++)
+            //for(int coluna = 0; coluna < 3 ; coluna++)
+                
+                //linha
+                if((mapa[0][0] == jogador && mapa[1][0] == jogador && mapa[2][0] == jogador) ||
+                    (mapa[0][1] == jogador && mapa[1][1] == jogador && mapa[2][1] == jogador) ||
+                    (mapa[0][2] == jogador && mapa[1][2] == jogador && mapa[2][2] == jogador))
+                     return true;
+                
+                //coluna
+                if((mapa[0][0] == jogador && mapa[0][1] == jogador && mapa[0][2] == jogador) ||
+                    (mapa[1][0] == jogador && mapa[1][1] == jogador && mapa[1][2] == jogador) ||
+                    (mapa[2][0] == jogador && mapa[2][1] == jogador && mapa[2][2] == jogador))
+                     return true;
+                
+                //diagonal
+                if((mapa[0][0] == jogador && mapa[1][1] == jogador && mapa[2][2] == jogador) ||
+                    (mapa[0][2] == jogador && mapa[1][1] == jogador && mapa[2][0] == jogador))
+                     return true;
+        return false;
+   }
+  
 }
-
