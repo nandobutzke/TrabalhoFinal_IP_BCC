5912c8fd06a288f63e0d8bb3da2e2869b9053dbb
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-15 23:16:01 -0300

    Update JogoDaVelha_PC.java

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -1,19 +1,14 @@
+
 public class JogoDaVelha_PC {
 
- 
-
     public JogoDaVelha_Mapa mapa;
-    public static char letra = 'O';        //Usar letra 'O'
+    public static char letra = 'O';        
     
     public JogoDaVelha_PC(JogoDaVelha_Mapa mapa) {
         this.mapa = mapa;
     }
-    
-    
+
     public boolean joga() {
-        
-    	
-        
         int linha = mapa.sortear(0, 3);
         int coluna = mapa.sortear(0, 3);
         
@@ -22,16 +17,10 @@ public class JogoDaVelha_PC {
         boolean verificar = mapa.jogar(linha, coluna, letra);
         
         if(!verificar) {
-            System.out.println("jogada do PC inválida, campo ocupado");
             joga();
         } else {
         	System.out.println(posicaoPC);
         }
-        
-//        if(verificar) {
-//            return true;
-//        }
-        
         
         return true;
     }
