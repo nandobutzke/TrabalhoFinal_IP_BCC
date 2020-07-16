5b650317e99b5a2c5ec55c025a34de633a17edb8
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-16 17:25:45 -0300

    Update JogoDaVelha_Jogador.java

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Jogador.java
@@ -12,22 +12,25 @@ public class JogoDaVelha_Jogador {
 
         int linha, coluna;
         do {
-        	System.out.println("linha:");
-        	linha = teclado.nextInt();
+        	System.out.println("linha:");			
+        	linha = teclado.nextInt();				//recebe a posição da linha que receberá o caracter 'X'.
         	System.out.println("coluna:");
-        	coluna = teclado.nextInt();
+        	coluna = teclado.nextInt();				//recebe a posição da coluna que receberá o caracter 'X'.
         	
+        	/*Se o valor for maior que 2 ou menor que 0
+			tanto para as linhas quanto para as colunas
+			retornará "Fora da Caixa".	*/
         	if(linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
-            	System.out.println("Fora da caixa");
-            }
-        	
+            	System.out.println("Fora da caixa");					
+        	}															
+            															
         } while (linha < 0 || linha > 2 || coluna < 0 || coluna > 2);
 
         boolean verificar = mapa.jogar(linha, coluna, letra);
         
-        if(!verificar) {
+        if(!verificar) {												//caso o espaço estiver ocupado pelo caracter 'X', irá executar o método joga novamente
             System.out.println("jogada inválida, campo ocupado");
-            joga(teclado);
+            joga(teclado);												//executa o método jogar novamente
         }
 
         return true;
