0d9f79e9057006c575288232a2272c887f1532ae
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-16 17:24:26 -0300

    Update JogoDaVelha_PC.java

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_PC.java
@@ -9,17 +9,17 @@ public class JogoDaVelha_PC {
     }
 
     public boolean joga() {
-        int linha = mapa.sortear(0, 3);
-        int coluna = mapa.sortear(0, 3);
+        int linha = mapa.sortear(0, 3);				//sorteia a linha em que será adicionado o caracter 'O' do PC.
+        int coluna = mapa.sortear(0, 3);			//sorteia a coluna em que será adiconado o caracter 'O' do PC.
         
-        String posicaoPC = "PC[" + linha + "," + coluna + "]";
+        String posicaoPC = "PC[" + linha + "," + coluna + "]";		//String que atribui a posição da jogada do PC
 
-        boolean verificar = mapa.jogar(linha, coluna, letra);
+        boolean verificar = mapa.jogar(linha, coluna, letra);		//variável e método que verificam se a posição recebe um espaço ' '. 
         
-        if(!verificar) {
+        if(!verificar) {		//caso o espaço estiver ocupado pelo caracter 'O', irá executar o método joga novamente
             joga();
         } else {
-        	System.out.println(posicaoPC);
+        	System.out.println(posicaoPC);			//escreve a posição da jogada caso a mesma não estiver ocupada
         }
         
         return true;
