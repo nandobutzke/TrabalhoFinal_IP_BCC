2249eeb55b148c662f920d6262386c226ab6d65a
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-07-16 17:26:16 -0300

    Update JogoDaVelha_Mapa.java

--- TrabalhoFinal_IP_BCC/src/JogoDaVelha_Mapa.java
+++ TrabalhoFinal_IP_BCC/src/JogoDaVelha_Mapa.java
@@ -1,21 +1,21 @@
 public class JogoDaVelha_Mapa {
     public static char[][] mapa = new char[3][3];
 
-    public int sortear(int inicio, int fim) {
-        return (int) ((Math.random()*fim)-inicio);
+    public int sortear(int inicio, int fim) {			//Escopo do método que sorteia as posições e controla a vez de cada jogador
+        return (int) ((Math.random()*fim)-inicio);		//Inclusive quem começa.
     }
     
     public void limpaMapa(){
         for(int linha = 0; linha < 3; linha++)
             for(int coluna = 0; coluna <3; coluna++)
-                mapa[linha][coluna] = ' ';
+                mapa[linha][coluna] = ' ';					//Adiciona o caracter ' ' a todas as posições do Mapa
     }
 
-    public void desenha(int jogada){
+    public void desenha(int jogada){						//Desenha o mapa com as respectivas posicões e caracteres.
         
-        System.out.print("-------------");
+        System.out.print("-------------");															
         System.out.println(" jogada: " + jogada);
-        System.out.println("| " + mapa[0][0] + " | " + mapa[0][1] + " | " + mapa[0][2] + " | ");
+        System.out.println("| " + mapa[0][0] + " | " + mapa[0][1] + " | " + mapa[0][2] + " | ");		
         System.out.println("-------------");
         System.out.println("| " + mapa[1][0] + " | " + mapa[1][1] + " | " + mapa[1][2] + " | ");
         System.out.println("-------------");
@@ -24,8 +24,8 @@ public class JogoDaVelha_Mapa {
         
     }
 
-    public boolean jogar(int l, int c, char jogador) {
-	
+    public boolean jogar(int l, int c, char jogador) {	//Verifica se a posição se encontra com o caracter ' '(se está apta a receber um caracter
+    													//'X' ou 'O').
     	if(mapa[l][c] == ' ') {
         	mapa[l][c] = jogador;
             return true;
@@ -35,7 +35,7 @@ public class JogoDaVelha_Mapa {
 
     }
     
-    public boolean ganhou(char jogador){
+    public boolean ganhou(char jogador){		 //Método que verifica (diagonais, linhas e colunas) se o PC ou jogador venceu.
                 
                 //coluna
                 if((mapa[0][0] == jogador && mapa[1][0] == jogador && mapa[2][0] == jogador) ||
